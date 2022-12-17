69e4c6b690eaa2ad53db262401be8109d9d7ce06
Dmytro1980 <dm.st.mazda@gmail.com>
2022-12-17 12:01:35 +0200

    edit: delete odd files

--- src/ArrayDataException02.java
+++
@@ -1,6 +0,0 @@
-public class ArrayDataException02 extends RuntimeException {
-
-    public ArrayDataException02() {
-        System.out.println("ArrayDataException says: Wrong array element");
-    }
-}
